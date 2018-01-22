47017112ec04992556c72a5cf2b39aa8314fea8d
TheDude12 <valsamak@gmail.com>
2018-01-22 23:12:21 +0200

    Add files via upload

--- mydb.sql
+++ mydb.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100129
 File Encoding         : 65001
 
-Date: 2018-01-22 22:56:15
+Date: 2018-01-22 23:11:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,83 +20,65 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
-`username`  char(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`pass`  char(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `username` char(255) DEFAULT NULL,
+  `pass` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of accounts
 -- ----------------------------
-BEGIN;
-INSERT INTO `accounts` VALUES ('asdasd', ''), ('valsas', 'peos'), ('stelios', 'peos');
-COMMIT;
+INSERT INTO `accounts` VALUES ('asdasd', '');
+INSERT INTO `accounts` VALUES ('valsas', 'peos');
+INSERT INTO `accounts` VALUES ('stelios', 'peos');
 
 -- ----------------------------
 -- Table structure for armor
 -- ----------------------------
 DROP TABLE IF EXISTS `armor`;
 CREATE TABLE `armor` (
-`Name`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`Bid`  float(10,0) NULL DEFAULT NULL ,
-`Buyout`  float(255,0) NULL DEFAULT NULL ,
-`user`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `Name` varchar(255) DEFAULT NULL,
+  `Bid` float(10,0) DEFAULT NULL,
+  `Buyout` float(255,0) DEFAULT NULL,
+  `user` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of armor
 -- ----------------------------
-BEGIN;
-INSERT INTO `armor` VALUES ('dragonscale', '10', '1000', 'stelios'), ('poutsoscale', '0', '1', 'valsas');
-COMMIT;
+INSERT INTO `armor` VALUES ('dragonscale', '10', '1000', 'stelios');
+INSERT INTO `armor` VALUES ('poutsoscale', '0', '1', 'valsas');
 
 -- ----------------------------
 -- Table structure for trinkets
 -- ----------------------------
 DROP TABLE IF EXISTS `trinkets`;
 CREATE TABLE `trinkets` (
-`Name`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`Bid`  float(10,0) NULL DEFAULT NULL ,
-`Buyout`  float(255,0) NULL DEFAULT NULL ,
-`user`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `Name` varchar(255) DEFAULT NULL,
+  `Bid` float(10,0) DEFAULT NULL,
+  `Buyout` float(255,0) DEFAULT NULL,
+  `user` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of trinkets
 -- ----------------------------
-BEGIN;
-INSERT INTO `trinkets` VALUES ('peos', '3', '34', 'stelios'), ('teeth', '1', '25', 'valsas');
-COMMIT;
+INSERT INTO `trinkets` VALUES ('peos', '3', '34', 'stelios');
+INSERT INTO `trinkets` VALUES ('teeth', '1', '25', 'valsas');
 
 -- ----------------------------
 -- Table structure for weapons
 -- ----------------------------
 DROP TABLE IF EXISTS `weapons`;
 CREATE TABLE `weapons` (
-`Name`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`Bid`  float(10,0) NULL DEFAULT NULL ,
-`Buyout`  float(255,0) NULL DEFAULT NULL ,
-`user`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-
-;
+  `Name` varchar(255) DEFAULT NULL,
+  `Bid` float(10,0) DEFAULT NULL,
+  `Buyout` float(255,0) DEFAULT NULL,
+  `user` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 -- ----------------------------
 -- Records of weapons
 -- ----------------------------
-BEGIN;
-INSERT INTO `weapons` VALUES ('sword', '1', '5', 'stelios'), ('gun', '3', '7', 'valsas'), ('bow', '3', '50', 'stelios');
-COMMIT;
+INSERT INTO `weapons` VALUES ('sword', '1', '5', 'stelios');
+INSERT INTO `weapons` VALUES ('gun', '3', '7', 'valsas');
+INSERT INTO `weapons` VALUES ('bow', '3', '50', 'stelios');
