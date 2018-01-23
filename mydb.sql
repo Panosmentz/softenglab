4e23a9123376d4bc93b3289d39d3d3ede1c184f5
Panagiotis Mentzelopoulos <33544443+Panosmentz@users.noreply.github.com>
2018-01-23 16:39:30 +0200

    Add files via upload

--- mydb.sql
+++ mydb.sql
@@ -2,21 +2,21 @@
 Navicat MySQL Data Transfer
 
 Source Server         : MyDB
-Source Server Version : 100129
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : mydb
 
 Target Server Type    : MYSQL
-Target Server Version : 100129
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-01-22 23:11:30
+Date: 2018-01-23 16:39:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for accounts
+-- Table structure for `accounts`
 -- ----------------------------
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
@@ -30,9 +30,11 @@ CREATE TABLE `accounts` (
 INSERT INTO `accounts` VALUES ('asdasd', '');
 INSERT INTO `accounts` VALUES ('valsas', 'peos');
 INSERT INTO `accounts` VALUES ('stelios', 'peos');
+INSERT INTO `accounts` VALUES ('panos', 'peos');
+INSERT INTO `accounts` VALUES ('vlas', '1234');
 
 -- ----------------------------
--- Table structure for armor
+-- Table structure for `armor`
 -- ----------------------------
 DROP TABLE IF EXISTS `armor`;
 CREATE TABLE `armor` (
@@ -46,10 +48,10 @@ CREATE TABLE `armor` (
 -- Records of armor
 -- ----------------------------
 INSERT INTO `armor` VALUES ('dragonscale', '10', '1000', 'stelios');
-INSERT INTO `armor` VALUES ('poutsoscale', '0', '1', 'valsas');
+INSERT INTO `armor` VALUES ('brigadine', '3000', '10000', 'vlas');
 
 -- ----------------------------
--- Table structure for trinkets
+-- Table structure for `trinkets`
 -- ----------------------------
 DROP TABLE IF EXISTS `trinkets`;
 CREATE TABLE `trinkets` (
@@ -66,7 +68,7 @@ INSERT INTO `trinkets` VALUES ('peos', '3', '34', 'stelios');
 INSERT INTO `trinkets` VALUES ('teeth', '1', '25', 'valsas');
 
 -- ----------------------------
--- Table structure for weapons
+-- Table structure for `weapons`
 -- ----------------------------
 DROP TABLE IF EXISTS `weapons`;
 CREATE TABLE `weapons` (
@@ -82,3 +84,4 @@ CREATE TABLE `weapons` (
 INSERT INTO `weapons` VALUES ('sword', '1', '5', 'stelios');
 INSERT INTO `weapons` VALUES ('gun', '3', '7', 'valsas');
 INSERT INTO `weapons` VALUES ('bow', '3', '50', 'stelios');
+INSERT INTO `weapons` VALUES ('shotgun', '3', '60', 'panos');
