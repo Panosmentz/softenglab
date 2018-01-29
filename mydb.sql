91659235c2fcbc394e85a30232e0061ac346c82a
Panagiotis Mentzelopoulos <33544443+Panosmentz@users.noreply.github.com>
2018-01-29 23:57:10 +0200

    Add files via upload

--- mydb.sql
+++ mydb.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2018-01-28 20:26:20
+Date: 2018-01-29 23:55:54
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,86 +20,38 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
-  `username` char(255) DEFAULT NULL,
-  `pass` char(255) DEFAULT NULL
+  `username` char(255) NOT NULL,
+  `pass` char(255) NOT NULL,
+  `balance` float(255,0) NOT NULL,
+  `adflg` int(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of accounts
 -- ----------------------------
-INSERT INTO `accounts` VALUES ('asdasd', '');
-INSERT INTO `accounts` VALUES ('valsas', 'peos');
-INSERT INTO `accounts` VALUES ('stelios', 'peos');
-INSERT INTO `accounts` VALUES ('panos', 'peos');
-INSERT INTO `accounts` VALUES ('vlas', '1234');
+INSERT INTO `accounts` VALUES ('stelios', 'peos', '1000', '0');
+INSERT INTO `accounts` VALUES ('panos', 'peos', '641', '0');
+INSERT INTO `accounts` VALUES ('vlas', '1234', '1000', '0');
+INSERT INTO `accounts` VALUES ('admin', 'root', '0', '1');
+INSERT INTO `accounts` VALUES ('lol', 'lol', '1000', '0');
+INSERT INTO `accounts` VALUES ('valsas', 'peos', '1000', '0');
 
 -- ----------------------------
--- Table structure for `adminaccounts`
+-- Table structure for `products`
 -- ----------------------------
-DROP TABLE IF EXISTS `adminaccounts`;
-CREATE TABLE `adminaccounts` (
-  `adName` varchar(255) NOT NULL,
-  `adPass` varchar(255) NOT NULL
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `Name` varchar(255) NOT NULL,
+  `Bid` float(255,0) NOT NULL,
+  `Buyout` float(255,0) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `EndBid` float(255,0) NOT NULL,
+  `Type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of adminaccounts
+-- Records of products
 -- ----------------------------
-INSERT INTO `adminaccounts` VALUES ('admin', 'root');
-INSERT INTO `adminaccounts` VALUES ('valsasad', 'lalala12');
-
--- ----------------------------
--- Table structure for `armor`
--- ----------------------------
-DROP TABLE IF EXISTS `armor`;
-CREATE TABLE `armor` (
-  `Name` varchar(255) DEFAULT NULL,
-  `Bid` float(10,0) DEFAULT NULL,
-  `Buyout` float(255,0) DEFAULT NULL,
-  `user` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Records of armor
--- ----------------------------
-INSERT INTO `armor` VALUES ('dragonscale', '10', '1000', 'stelios');
-INSERT INTO `armor` VALUES ('brigadine', '3000', '10000', 'vlas');
-INSERT INTO `armor` VALUES ('exosuit', '1200', '10000', 'stelios');
-
--- ----------------------------
--- Table structure for `trinkets`
--- ----------------------------
-DROP TABLE IF EXISTS `trinkets`;
-CREATE TABLE `trinkets` (
-  `Name` varchar(255) DEFAULT NULL,
-  `Bid` float(10,0) DEFAULT NULL,
-  `Buyout` float(255,0) DEFAULT NULL,
-  `user` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Records of trinkets
--- ----------------------------
-INSERT INTO `trinkets` VALUES ('peos', '3', '34', 'stelios');
-INSERT INTO `trinkets` VALUES ('teeth', '1', '25', 'valsas');
-INSERT INTO `trinkets` VALUES ('locket', '2', '20', 'stelios');
-
--- ----------------------------
--- Table structure for `weapons`
--- ----------------------------
-DROP TABLE IF EXISTS `weapons`;
-CREATE TABLE `weapons` (
-  `Name` varchar(255) DEFAULT NULL,
-  `Bid` float(10,0) DEFAULT NULL,
-  `Buyout` float(255,0) DEFAULT NULL,
-  `user` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Records of weapons
--- ----------------------------
-INSERT INTO `weapons` VALUES ('sword', '5', '5', 'stelios');
-INSERT INTO `weapons` VALUES ('bow', '3', '50', 'stelios');
-INSERT INTO `weapons` VALUES ('shotgun', '3', '60', 'panos');
-INSERT INTO `weapons` VALUES ('valsa_ball', '1', '10', 'valsas');
-INSERT INTO `weapons` VALUES ('valsa ball', '1', '10', 'valsas');
+INSERT INTO `products` VALUES ('sword', '19', '100', 'stelios', '10', 'Weapons');
+INSERT INTO `products` VALUES ('teeth', '3', '300', 'stelios', '30', 'Trinkets');
+INSERT INTO `products` VALUES ('o poutsos mou', '100', '100', 'valsas', '50', 'Weapons');
